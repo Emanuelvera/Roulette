cc13ded8822345b0684e9ea01d3d585b7dbcece7
Emanuelvera <emanuelvera03@gmail.com>
2024-09-12 09:53:29 -0300

    fixed tests

--- src/email/email.controller.spec.ts
+++ src/email/email.controller.spec.ts
@@ -1,20 +1,37 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { EmailController } from './email.controller';
 import { EmailService } from './email.service';
+import { ConfigService } from '@nestjs/config'; // Importa el ConfigService
 
 describe('EmailController', () => {
   let controller: EmailController;
+  let emailService: EmailService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [EmailController],
-      providers: [EmailService],
+      providers: [
+        {
+          provide: EmailService,
+          useValue: {
+            sendEmail: jest.fn(() => 'Email sent'), // Simula el método que quieras
+          },
+        },
+        {
+          provide: ConfigService,
+          useValue: {
+            get: jest.fn(() => 'mocked value'), // Mockea el ConfigService si es necesario
+          },
+        },
+      ],
     }).compile();
 
     controller = module.get<EmailController>(EmailController);
+    emailService = module.get<EmailService>(EmailService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+    expect(emailService).toBeDefined();
   });
 });
